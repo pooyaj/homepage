f7a5edd5802de64bf951ed32c14970c30c55c54a
Pooya Jaferian <pooya.j@gmail.com>
2020-03-02 23:05:46 -0800

    adding GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 const path = require(`path`);
-require('dotenv').config({
+require("dotenv").config({
   path: `.env`
 });
 
@@ -9,21 +9,41 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Pooya Jaferian',
-        short_name: 'Pooya',
-        start_url: '/',
-        background_color: '#6b37bf',
-        theme_color: '#6b37bf',
-        display: 'minimal-ui'
+        name: "Pooya Jaferian",
+        short_name: "Pooya",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "minimal-ui"
       }
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-offline`,
     {
-      resolve: 'gatsby-source-prismic',
+      resolve: "gatsby-source-prismic",
       options: {
-        repositoryName: 'pooya',
+        repositoryName: "pooya",
         accessToken: process.env.API_KEY
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-34710465-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "0",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10
       }
     }
   ]
